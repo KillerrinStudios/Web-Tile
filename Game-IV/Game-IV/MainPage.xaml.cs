2b704dac8068a0d519eb437df4b6e4b19f3fbe2c
Andrew Godfroy <andrew.godfroy@killerrin.com>
2015-01-20 19:21:45 -0500

    Initial Push

---
+++ Game-IV/Game-IV/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Game_IV.Resources;
+using Microsoft.Phone.Tasks;
+
+namespace Game_IV
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        static bool appLoaded = false;
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            if (appLoaded == false)
+            {
+                appLoaded = true;
+
+                NavigationService.RemoveBackEntry();
+
+                WebBrowserTask webTask = new WebBrowserTask();
+                webTask.Uri = new Uri("http://www.game-iv.com");
+                webTask.Show();
+            }
+
+            Application.Current.Terminate();
+        }
+    }
+}
